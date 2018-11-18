aebea5175bdf451e8cecb5b84aadac654adc3fee
YangJagon <y.1.jagon@gmail.com>
2018-11-19 01:20:39 +0800

    finish study

---
+++ home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Database Management</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="database.css" type="text/css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <h1>Database Management System</h1>
+            </header>
+
+            <nav>
+                <ul>
+                    <li> <a href="/database/home.php">HOME</a> </li>
+                    <li style="margin-top: 20px"> <a href="/database/index.php">CONTROL</a> </li>
+                </ul>
+            </nav>
+
+            <main>
+                <h2 class = "text">
+                    There are still <br>
+                    <?php
+                        $date = new DateTime('2019-1-21');      //创建寒假时间的类对象
+                        $date1 = new DateTime(date("ymd"));     //得到现在的时间的类对象
+                        $interval = $date1->diff($date);        //相减得到相差天数并显示
+                        echo "<span style=\"color: rgb(213, 84, 87); font-size: 1.3em\">".$interval->format('%a')."</span>";
+                    ?> 
+                     days<br>
+                    to the winter vacation!!!
+                </h2>
+                <hr style="margin-top: 30px">
+                <div style="width: 500px; margin: 30px auto;">
+                    <img src="./image/holiday.jpg" alt="holidays image" width="500">
+                </div>
+                <hr>
+            </main>
+
+            <footer>
+                Copyright &copy; 2018 Jagon
+            </footer>
+        </div>
+    </body>
+
+</html>
---
+++ rupdate.php
@@ -0,0 +1,30 @@
+<?php
+var_dump($_POST);
+$tablename=$_POST["tablename"];
+$idn=$_POST["idname"];
+$idd=$_POST["iddata"];
+
+include "initial.php";
+global $mysqli;
+$fields = get_table_fields($tablename);
+$types = get_table_types($tablename);
+$data = get_table_data($tablename);
+$str="";
+
+for($i=0; $i < count($fields); $i++)
+{
+    if($i>0)
+        $str=$str.", ";
+    $argv=$_POST[$fields[$i]];
+
+    if($argv == null)
+        $argv = "null";
+    else if(stripos("1".$types[$i], "date") || stripos("1".$types[$i], "char"))
+            $argv = "'".$argv."'";
+
+    $str = $str.$fields[$i]."=".$argv;
+}
+
+$res = mysqli_query($mysqli, "UPDATE ".$tablename." SET ".$str." WHERE ".$idn."=".$idd);
+header("Location: http://127.0.0.1/database/index.php?".$tablename);  
+?>
